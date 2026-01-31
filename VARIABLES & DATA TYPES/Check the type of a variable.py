03f8ccbc57f71b4fed7d99f573179580396288f3
Tanishk <tanishk2300@gmail.com>
2026-01-31 11:34:41 +0530

    dsa

---
+++ VARIABLES & DATA TYPES/Check the type of a variable.py
@@ -0,0 +1,8 @@
+age=25
+print(type(age)) # output "<class 'int'>" .
+number=39.98
+print(type(number)) # output "<class 'float'>" .
+name="tanishk"
+print(type(name)) #output "<class 'str'>" .
+is_active=True
+print(type(is_active)) #output "<class 'bool'>" .
---
+++ VARIABLES & DATA TYPES/Store and print different data types.py
@@ -0,0 +1,12 @@
+age=25 # storing a integer.
+print(age+5) # output 30 .
+price=19.99 # storing a float number .(decimal)
+print(price) #output 19,99 (as a float number).
+name="tanishk"# storing a string .(text)
+print(name) # output tanishk (text).
+is_active=True #storing a boolean. (logic)
+print(is_active) #output true (logic).
+
+
+
+
